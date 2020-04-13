a6b0adf0ba6d66ddbd8b1c9b55a98175ce9eb321
dilpri <dilpripunchihewa@gmail.com>
2020-04-13 17:27:37 +0530

    weight allocation and control structure navigation finished

--- CodeComplexityMeasuringTool/src/my/CodeComplexityCalculator/ControlStructures.java
+++ CodeComplexityMeasuringTool/src/my/CodeComplexityCalculator/ControlStructures.java
@@ -18,6 +18,7 @@ public class ControlStructures extends javax.swing.JFrame {
         initComponents();
     }
 
+    ControlStructureWeights cs = new ControlStructureWeights();
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -161,6 +162,7 @@ public class ControlStructures extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
+        cs.setVisible(true);
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
